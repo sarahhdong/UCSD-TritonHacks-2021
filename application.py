e75393dead4df3427ef3376d556efbd51c5385c9
annabelng <a.h.ng952@gmail.com>
2021-06-27 11:13:24 -0700

    trying to add hotel

--- application.py
+++ application.py
@@ -6,7 +6,7 @@ import webbrowser
 import sys        # Used to end program if error occurs
 
 from convert import avg_price, avg_rating
-from scraper import scrape_for
+from scraper import scrape_for, getHotel
 from scraper import getweather
 from generator import generate, homePage
 
@@ -21,13 +21,35 @@ def index():
 def introduction():
     return render_template('introduction.html')
 
+@app.route('/hotel', methods=('GET', 'POST'))
+def hotel():
+    if request.method == 'POST':
+        city1 = request.form['city1']
+        city2 = request.form['city2']
+
+        if not (city1 and city2):
+            flash('Required field needed')
+        else:
+            try:
+                (hotels1, prices1, imgs1) = getHotel(city1)
+                (hotel2, prices2, imgs2) = getHotel(city2)
+
+                city1_avg = avg_price(prices1)
+                city2_avg = avg_prices(prices2)
+
+                cheaper = place2 if hotel1_avg_price > hotel2_avg_price else place1
+                return (cheaper + " is cheaper on average")
+            except Exception as e:
+                flash('Bad input, please re-enter')
+
+
 @app.route('/compare', methods=('GET', 'POST'))
 def compare():
     if request.method == 'POST':
         type = request.form['type']
         loc1 = request.form['loc1']
         loc2 = request.form['loc2']
-        
+
         # Make the scrapping requests (this may take a few seconds)
         # scrape_for is a function imported from scraper.py
         if not (type and loc1 and loc2):
@@ -36,30 +58,30 @@ def compare():
             try:
                 (prices1, stars1, images1) = scrape_for(type, loc1)
                 (prices2, stars2, images2) = scrape_for(type, loc2)
-        
+
                 # calculate the averaged prices and stars of the separate locations
                 place1_avg_price = avg_price(prices1)
                 place1_avg_rating = avg_rating(stars1)
 
                 place2_avg_price = avg_price(prices2)
                 place2_avg_rating = avg_rating(stars2)
-            
+
             except Exception as e:
                 flash('Bad input, please re-enter.')
                 return render_template('compare.html')
                 #return("Input value is not corret. Please re-enter")
-        
+
             # Determine the cheaper and higher rated places
             #cheaper = loc2 if place1_avg_price > place2_avg_price else loc1
             #higher_rating = loc2 if place2_avg_rating > place1_avg_rating else loc1
-            
+
             com
-            
-            
+
+
             return redirect(url_for('result', type=type, loc1=loc1, loc2=loc2, place1_avg_price=place1_avg_price, place2_avg_price=place2_avg_price, place1_avg_rating=place1_avg_rating, place2_avg_rating=place2_avg_rating, cheaper=cheaper, higher=higher_rating))
             #return 'Cheaper place is: %s' % cheaper + '   Higher rating is: %s' % higher_rating
             #return 'star1 is: %s' % cheaper + '   Higher rating is: %s' % higher_rating
-        
+
     return render_template('compare.html')
 
 @app.route('/result/<type>/<loc1>/<loc2>/<place1_avg_price>/<place2_avg_price>/<place1_avg_rating>/<place2_avg_rating>/ <cheaper>/<higher>')
--- scraper.py
+++ scraper.py
@@ -24,7 +24,7 @@ def scrape_for(query, location, ):
 
   # Get all search results
   results = scraper.find_all("div", class_="container__09f24__21w3G")
-  
+
   """
   PART 3
   hint: refer to results in (line 26)
@@ -57,7 +57,7 @@ def scrape_for(query, location, ):
 def getweather(location):
   # Builds the URL for what to scrape
   url ="https://www.timeanddate.com/weather/usa/"
-  url += location.lower() 
+  url += location.lower()
 
 
   # Scrape the url with BeautifulSoup
@@ -67,10 +67,38 @@ def getweather(location):
   # Get all search results - CHANGE
   info = scraper.find_all("div", class_="bk-focus__info")
   temp = scraper.find_all("div", class_ ="h2")
-  
+
   print(temp.next_sibling.strip())
   #desc = scraper.find_all("div", class ="h2")
   return temp
 
+def getHotel(city):
+  # Builds the URL for what to scrape
+  url = "https://www.booking.com/city/us/"
+  url += (city.lower() + '.html')
 
- 
+  # Scrape the url with BeautifulSoup
+  page = requests.get(url, headers)
+  #new_page = remove_tags(page.content)
+  scraper = BeautifulSoup(page.content, 'html.parser')
+
+  # Get top 10 hotels
+  hotels = scraper.find_all("span", itemprop = "name")
+
+  # turn hotels(array of html tags) into numbers
+  for i in range(len(hotels)):
+    hotels[i] = hotels[i].string.strip()
+
+  prices = scraper.find_all("div", class_ = "bui-price-display__value bui-f-color-constructive")
+
+  # turn prices(array of html tags) into numbers
+  for i in range(len(prices)):
+    prices[i] = prices[i].string
+
+  # Get all images, store only source attributes
+  images = scraper.find_all(class_="sr__card_photo")[:10]
+  image_sources = [img["src"] for img in images]
+
+  return hotels,prices, image_sources
+
+
